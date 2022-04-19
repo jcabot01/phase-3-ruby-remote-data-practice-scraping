3536f12e2405618acc6b0144569aaf2e9c40ebb6
JonCabot <jfc0053@gmail.com>
2022-04-18 21:49:41 -0500

    completed assignment

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,3 +2,10 @@ require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+
+doc = Nokogiri::HTML(html)
+courses = doc.css("#panelSticky")
+
+courses.each do |course|
+  puts course.text.strip
+end
